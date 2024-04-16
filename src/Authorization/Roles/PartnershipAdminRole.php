2bcd8d8e66f75860ae38c9163d20089a351403c5
Harun Baris Bulut <baris.bulut@plusclouds.com>
2024-04-16 20:45:20 +0300

    New get rules function added to authorization

--- src/Authorization/Roles/PartnershipAdminRole.php
+++ src/Authorization/Roles/PartnershipAdminRole.php
@@ -30,14 +30,7 @@ class PartnershipAdminRole extends AbstractRole implements IAuthorizationRole
      */
     public function apply(Builder $builder, Model $model)
     {
-        /**
-         * Here user will be able to list all models, because by default, sales manager can see everybody.
-         */
-        $ids = AccountManagers::withoutGlobalScopes()
-            ->where('iam_account_id', UserHelper::currentAccount()->id)
-            ->pluck('crm_account_id');
 
-        $builder->whereIn('iam_account_id', $ids);
     }
 
     public function checkPrivileges(Users $users = null)
@@ -108,5 +101,10 @@ class PartnershipAdminRole extends AbstractRole implements IAuthorizationRole
     public function getDbPrefix()
     {
         return self::DB_PREFIX;
+    }
+
+    public function checkRules(Users $users): bool
+    {
+        // TODO: Implement checkRules() method.
     }
 }
