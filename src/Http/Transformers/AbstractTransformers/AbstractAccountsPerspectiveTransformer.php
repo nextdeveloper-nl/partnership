2965742ef085afb76cec58da8cf57de8be22f485
Harun Baris Bulut <baris.bulut@plusclouds.com>
2024-11-06 23:23:30 +0300

    Accounts perspective transformer is fixed

--- src/Http/Transformers/AbstractTransformers/AbstractAccountsPerspectiveTransformer.php
+++ src/Http/Transformers/AbstractTransformers/AbstractAccountsPerspectiveTransformer.php
@@ -54,54 +54,54 @@ class AbstractAccountsPerspectiveTransformer extends AbstractTransformer
      */
     public function transform(AccountsPerspective $model)
     {
-                                                $iamAccountId = \NextDeveloper\IAM\Database\Models\Accounts::where('id', $model->iam_account_id)->first();
-                                                            $iamAccountTypeId = \NextDeveloper\IAM\Database\Models\AccountTypes::where('id', $model->iam_account_type_id)->first();
-                                                            $commonDomainId = \NextDeveloper\Commons\Database\Models\Domains::where('id', $model->common_domain_id)->first();
-                                                            $commonCountryId = \NextDeveloper\Commons\Database\Models\Countries::where('id', $model->common_country_id)->first();
-                                                            $iamUserId = \NextDeveloper\IAM\Database\Models\Users::where('id', $model->iam_user_id)->first();
-                                                            $distributorId = \NextDeveloper\\Database\Models\Distributors::where('id', $model->distributor_id)->first();
-                        
+        $iamAccountId = \NextDeveloper\IAM\Database\Models\Accounts::where('id', $model->iam_account_id)->first();
+        $iamAccountTypeId = \NextDeveloper\IAM\Database\Models\AccountTypes::where('id', $model->iam_account_type_id)->first();
+        $commonDomainId = \NextDeveloper\Commons\Database\Models\Domains::where('id', $model->common_domain_id)->first();
+        $commonCountryId = \NextDeveloper\Commons\Database\Models\Countries::where('id', $model->common_country_id)->first();
+        $iamUserId = \NextDeveloper\IAM\Database\Models\Users::where('id', $model->iam_user_id)->first();
+        $distributorId = \NextDeveloper\Partnership\Database\Models\Accounts::where('id', $model->distributor_id)->first();
+
         return $this->buildPayload(
             [
-            'id'  =>  $model->uuid,
-            'name'  =>  $model->name,
-            'description'  =>  $model->description,
-            'iam_account_id'  =>  $iamAccountId ? $iamAccountId->uuid : null,
-            'iam_account_type_id'  =>  $iamAccountTypeId ? $iamAccountTypeId->uuid : null,
-            'account_type'  =>  $model->account_type,
-            'common_domain_id'  =>  $commonDomainId ? $commonDomainId->uuid : null,
-            'domain_name'  =>  $model->domain_name,
-            'common_country_id'  =>  $commonCountryId ? $commonCountryId->uuid : null,
-            'country_name'  =>  $model->country_name,
-            'iam_user_id'  =>  $iamUserId ? $iamUserId->uuid : null,
-            'account_owner'  =>  $model->account_owner,
-            'distributor_id'  =>  $distributorId ? $distributorId->uuid : null,
-            'distributor'  =>  $model->distributor,
-            'partner_code'  =>  $model->partner_code,
-            'is_brand_ambassador'  =>  $model->is_brand_ambassador,
-            'payable_income'  =>  $model->payable_income,
-            'customer_count'  =>  $model->customer_count,
-            'iban'  =>  $model->iban,
-            'level'  =>  $model->level,
-            'reward_points'  =>  $model->reward_points,
-            'boosts'  =>  $model->boosts,
-            'mystery_box'  =>  $model->mystery_box,
-            'badges'  =>  $model->badges,
-            'technical_capabilities'  =>  $model->technical_capabilities,
-            'industry'  =>  $model->industry,
-            'sector_focus'  =>  $model->sector_focus,
-            'special_interest'  =>  $model->special_interest,
-            'compliance_certifications'  =>  $model->compliance_certifications,
-            'is_reseller'  =>  $model->is_reseller,
-            'is_integrator'  =>  $model->is_integrator,
-            'is_distributor'  =>  $model->is_distributor,
-            'is_vendor'  =>  $model->is_vendor,
-            'is_affiliate'  =>  $model->is_affiliate,
-            'target_group'  =>  $model->target_group,
-            'meeting_link'  =>  $model->meeting_link,
-            'created_at'  =>  $model->created_at,
-            'updated_at'  =>  $model->updated_at,
-            'deleted_at'  =>  $model->deleted_at,
+                'id' => $model->uuid,
+                'name' => $model->name,
+                'description' => $model->description,
+                'iam_account_id' => $iamAccountId ? $iamAccountId->uuid : null,
+                'iam_account_type_id' => $iamAccountTypeId ? $iamAccountTypeId->uuid : null,
+                'account_type' => $model->account_type,
+                'common_domain_id' => $commonDomainId ? $commonDomainId->uuid : null,
+                'domain_name' => $model->domain_name,
+                'common_country_id' => $commonCountryId ? $commonCountryId->uuid : null,
+                'country_name' => $model->country_name,
+                'iam_user_id' => $iamUserId ? $iamUserId->uuid : null,
+                'account_owner' => $model->account_owner,
+                'distributor_id' => $distributorId ? $distributorId->uuid : null,
+                'distributor' => $model->distributor,
+                'partner_code' => $model->partner_code,
+                'is_brand_ambassador' => $model->is_brand_ambassador,
+                'payable_income' => $model->payable_income,
+                'customer_count' => $model->customer_count,
+                'iban' => $model->iban,
+                'level' => $model->level,
+                'reward_points' => $model->reward_points,
+                'boosts' => $model->boosts,
+                'mystery_box' => $model->mystery_box,
+                'badges' => $model->badges,
+                'technical_capabilities' => $model->technical_capabilities,
+                'industry' => $model->industry,
+                'sector_focus' => $model->sector_focus,
+                'special_interest' => $model->special_interest,
+                'compliance_certifications' => $model->compliance_certifications,
+                'is_reseller' => $model->is_reseller,
+                'is_integrator' => $model->is_integrator,
+                'is_distributor' => $model->is_distributor,
+                'is_vendor' => $model->is_vendor,
+                'is_affiliate' => $model->is_affiliate,
+                'target_group' => $model->target_group,
+                'meeting_link' => $model->meeting_link,
+                'created_at' => $model->created_at,
+                'updated_at' => $model->updated_at,
+                'deleted_at' => $model->deleted_at,
             ]
         );
     }
